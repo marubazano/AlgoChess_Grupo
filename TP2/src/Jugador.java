7f28168dcd1da0ef911b8e3928d311cb52d44afb
Joaquin Hojman <hojmanjoaquin@gmail.com>
2019-10-29 17:04:50 -0300

    Agregue Clase Jugador y clase Unidad (sin programar nada)

--- TP2/src/Jugador.java
+++ TP2/src/Jugador.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 public class Jugador {
     private String nombre;
-    private ArrayList<String> unidades; //cambiar de tipo string a tipo unidad
+    private ArrayList<Unidad> unidades;
 
     Jugador(String nombre) {
         this.nombre = nombre;
-        this.unidades= new ArrayList<String>(); //cambiar de tipo string a tipo unidad
+        this.unidades= new ArrayList<Unidad>();
     }
-    public void agregarUnidadAJugador(String unidad) { //cambiar de tipo string a tipo unidad
+    public void agregarUnidadAJugador(Unidad unidad) {
         this.unidades.add(unidad);
     }
 
