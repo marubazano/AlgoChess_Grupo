ed425aacce3502ec46f547add0012959b00e4347
Marina Bazano <marinabazano@gmail.com>
2019-11-01 13:11:42 -0300

    Agregué unas cosas del diagrama de clases

--- TP2/tests/JugadorTest.java
+++ TP2/tests/JugadorTest.java
@@ -14,4 +14,20 @@ public class JugadorTest {
         pruebaJugador.agregarUnidadAJugador(prueba);
         Assert.assertEquals(pruebaJugador.obtenerCantidadUnidades(), 1);
     }
+
+    @Test
+    public void seAgregaUnidadesAlJugador(){
+        Unidad prueba_1 = new Unidad(0,0,null);
+        Unidad prueba_2 = new Unidad(0,0,null);
+        Unidad prueba_3 = new Unidad(0,0,null);
+        pruebaJugador.agregarUnidadAJugador(prueba_1);
+        pruebaJugador.agregarUnidadAJugador(prueba_2);
+        pruebaJugador.agregarUnidadAJugador(prueba_3);
+        Assert.assertEquals(pruebaJugador.obtenerCantidadUnidades(), 3);
+    }
+
+    @Test
+    public void jugadorNoTieneUnidades(){
+        Assert.assertEquals(pruebaJugador.obtenerCantidadUnidades(), 0);
+    }
 }
