262edd776c787f8505f89084f53f1ae0b248146c
Marina Bazano <marinabazano@gmail.com>
2019-11-19 15:11:16 -0300

    AHORA SE MUESTRA BIEN EL LAYOUT DE INGRESO

--- TP2/src/vista/Main.java
+++ TP2/src/vista/Main.java
@@ -6,6 +6,7 @@ import Tablero.Tablero;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
@@ -49,7 +50,8 @@ public class Main extends Application {
         window.setMinWidth(1000);
 
         Button jugar = new Button("Comenzar");
-        jugar.setStyle("-fx-font: 23 arial; -fx-base: #000000; -fx-border-color: #6d1fd8; -fx-text-fill: #f04ef5");
+        jugar.setPrefSize(250,90);
+        jugar.setStyle("-fx-font: 40 arial; -fx-base: #000000; -fx-border-color: #6d1fd8; -fx-text-fill: #f04ef5");
         Button salir = new Button("Salir");
         salir.setStyle("-fx-font: 23 arial; -fx-base: #000000; -fx-border-color: #ec443f; -fx-text-fill: #ec443f");
 
@@ -64,7 +66,7 @@ public class Main extends Application {
         layout.getChildren().add(jugar);
         layout.getChildren().add(salir);
         jugar.setLayoutX(400);
-        jugar.setLayoutY(450);
+        jugar.setLayoutY(500);
 
         jugar.setOnAction(e -> {
             try {
@@ -87,16 +89,18 @@ public class Main extends Application {
         window.setMaxWidth(1000);
         window.setMinWidth(1000);
 
+        Image titleBackground = new Image(getClass().getResourceAsStream("imagenes/menu.jpg"), 950, 800, false, true);
+        BackgroundImage imagenTitulo = new BackgroundImage(titleBackground, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
         Button ingresar = new Button("Ingresar");
+        ingresar.setPrefSize(250,90);
         ingresar.setStyle("-fx-font: 23 arial; -fx-base: #000000; -fx-border-color: #6d1fd8; -fx-text-fill: #f04ef5");
         Button salir = new Button("Salir");
         salir.setStyle("-fx-font: 23 arial; -fx-base: #000000; -fx-border-color: #ec443f; -fx-text-fill: #ec443f");
 
         Pane layout = new Pane ();
-
-        Image titleBackground = new Image(getClass().getResourceAsStream("imagenes/menu.jpg"), 950, 800, false, true);
-        BackgroundImage imagenTitulo = new BackgroundImage(titleBackground, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-                BackgroundSize.DEFAULT);
+        layout.setPrefSize(1000,700);
 
         TextField nombreJugador1 = new TextField("Jugador 1: Ingrese su nombre");
         TextField nombreJugador2 = new TextField("Jugador 2: Ingrese su nombre");
@@ -110,8 +114,8 @@ public class Main extends Application {
         nombreJugador1.setLayoutY(200);
         nombreJugador2.setLayoutX(400);
         nombreJugador2.setLayoutY(250);
-        ingresar.setLayoutX(450);
-        ingresar.setLayoutY(350);
+        ingresar.setLayoutX(400);
+        ingresar.setLayoutY(500);
 
         ingresar.setOnAction(e -> {
             try {
