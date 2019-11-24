75accf5024669f744c9ed23c17c107307444ceab
Marina Bazano <marinabazano@gmail.com>
2019-11-24 15:51:29 -0300

    Joaco y Mari.
    Seleccion de unidades con funcionalidad terminada.
    Jugar por turnos esta planteado, falta que leo agregue la movilidad y ataque, ademas falta que se hagan los updates por fuera.

--- TP2/src/vista/SeleccionDeUnidades.java
+++ TP2/src/vista/SeleccionDeUnidades.java
@@ -16,6 +16,7 @@ public class SeleccionDeUnidades {
         this.jugador2 = jugador2;
     }
 
+
     BotonUnidad gatoSoldado = new BotonUnidad("imagenes/gato_soldado_precio.png", "soldado", jugador1);
     BotonUnidad gatoJinete = new BotonUnidad("imagenes/gato_jinete_precio.png", "jinete", jugador1);
     BotonUnidad gatoCurandero = new BotonUnidad("imagenes/gato_curandero_precio.png", "curandero", jugador1);
@@ -27,7 +28,16 @@ public class SeleccionDeUnidades {
 
     BotonPasar pasar = new BotonPasar();
 
-    private void turnoJugador(Jugador jugador) {
+    public void turnoJugador(Jugador jugador) {  //se llama para cada jugador en el handle de botonComenzar
+
+        if (jugador == this.jugador1) {
+            desbloquearUnidadesJugador1();
+            bloquearUnidadesJugador2();
+        } else {
+            desbloquearUnidadesJugador2();
+            bloquearUnidadesJugador1();
+        }
+
         while (jugador.obtenerPuntos() > 0) {
 
             if(pasar.isPressed()) {
@@ -35,14 +45,49 @@ public class SeleccionDeUnidades {
             }
 
             if (jugador.obtenerPuntos() < 5 ) {
-
+                perroCatapulta.setDisable(true);
+                gatoCatapulta.setDisable(true);
             }
             if (jugador.obtenerPuntos() < 3 ) {
-
+                perroJinete.setDisable(true);
+                gatoJinete.setDisable(true);
             }
             if (jugador.obtenerPuntos() < 2 ) {
-
+                perroCurandero.setDisable(true);
+                gatoCurandero.setDisable(true);
             }
+
+            if (gatoSoldado.isPressed() || gatoJinete.isPressed() || gatoCurandero.isPressed() || gatoCatapulta.isPressed() || perroSoldado.isPressed() || perroJinete.isPressed() || perroCurandero.isPressed() || perroCatapulta.isPressed()) {
+                continue;
+            }  //feo feo dudoso
         }
+    }
+
+    public void bloquearUnidadesJugador2(){
+        perroSoldado.setDisable(true);
+        perroJinete.setDisable(true);
+        perroCurandero.setDisable(true);
+        perroCatapulta.setDisable(true);
+    }
+
+    public void desbloquearUnidadesJugador2(){
+        perroSoldado.setDisable(false);
+        perroJinete.setDisable(false);
+        perroCurandero.setDisable(false);
+        perroCatapulta.setDisable(false);
+    }
+
+    public void bloquearUnidadesJugador1(){
+        gatoSoldado.setDisable(true);
+        gatoJinete.setDisable(true);
+        gatoCurandero.setDisable(true);
+        gatoCatapulta.setDisable(true);
+    }
+
+    public void desbloquearUnidadesJugador1(){
+        gatoSoldado.setDisable(false);
+        gatoJinete.setDisable(false);
+        gatoCurandero.setDisable(false);
+        gatoCatapulta.setDisable(false);
     }
 }
